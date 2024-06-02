0ae74bcac989f2d8f42593dbedc0aa49ff48049b
paulo SA <paulo0704aragao@gmail.com>
2024-06-02 14:28:59 -0300

    nova alteracao

--- conta-banco/src/ContaTerminal1.java
+++ conta-banco/src/ContaTerminal1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal1 {
+
+    static int numero;
+    static String agencia;
+    static String nomeCliente;
+    static float saldo =237.48f;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero nome!");
+        nomeCliente = sc.nextLine();
+        System.out.println("Por favor, digite o numero da Agencia !");
+        agencia = sc.next();
+        System.out.println("Por favor, digite o numero da Conta");
+        numero = sc.nextInt();
+
+        System.out.println("Ola " + nomeCliente + " ,obrigado por criar uma conta em nosso banco, sua agencia e " +agencia + " conta " +numero + " e seu saldo "+ saldo +" já esta disponivel para saque");
+
+
     }
 }
